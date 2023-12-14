b5c4a25230fddbb6fa645d6f8df05a6cfd857aa9
arseniit <arseniit@gmail.com>
2023-12-14 09:21:42 +0100

    Add buttons, ngStyle and isAdmin toggle function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'CodeSandbox';
+  title = 'Bonjour !';
+  isAdmin = false;
+
+  toggleRole() {
+    this.isAdmin = !this.isAdmin;
+  }
 }
